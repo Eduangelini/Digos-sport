65c4c840a0c19c6684ffd8a3ea0d8f94dbb239e8
Eduardo <eduangelinicarvalho@gmail.com>
2023-04-28 16:58:39 -0300

    commit#5-responsivity-and-optimizations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,9 @@
-tailwind.config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
   theme: {
-    extend: {
-      screens: {
-        'snartphone': '360px',
-        'tablet': '768px',
-        'laptop': '1024px',
-        'desktop': '1440px',
-      },
-      colors: {
-        'primary': '#F52321',
-        'bg': '#FCFCFC',
-        'bg-card': '#F2F5FF',
-        'bg-footer': '#FFF2F2',
-
-        'black': '#000000',
-        'gray': '#DDDDDD',
-        'white': '#ffffff',
-      },
-      fontFamily: {
-        'serif': ['Poppins', 'serif'],
-      },
-    }
-  }
+    extend: {},
+  },
+  plugins: [],
 }
+
