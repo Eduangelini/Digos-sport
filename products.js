086bf91db28d2cd15df08bd6db37148c6fe38107
Eduardo <eduangelinicarvalho@gmail.com>
2023-05-15 15:54:52 -0300

    commit#18-add-products-and-links

--- products.js
+++ products.js
@@ -1,6 +1,11 @@
 // PRODUCTS LIST
 
 const produtos = [
+
+  // INICIO FUTEBOL, VOLEI E BASKET
+
+  // INICIO CHUTEIRA
+
   {
     imagem: './assets/products/futebol-chuteira-bravo.svg',
     titulo: 'Chuteira Penalty Bravo',
@@ -25,6 +30,52 @@ const produtos = [
     estrelas: 5,
     precoCartao: 239.90
   },
+  {
+    imagem: './assets/products/futebol-chuteira-classic-diavolo.svg',
+    titulo: 'Chuteira Society Diavolo Classic',
+    estrelas: 5,
+    precoCartao: 199.90
+  },
+  {
+    imagem: './assets/products/futebol-chuteira-precision-diavolo.svg',
+    titulo: 'Chuteira Society Diavolo Precision',
+    estrelas: 5,
+    precoCartao: 215.00
+  },
+  {
+    imagem: './assets/products/futebol-chuteira-society-dray.svg',
+    titulo: 'Chuteira Society Dray',
+    estrelas: 5,
+    precoCartao: 139.90
+  },
+  {
+    imagem: './assets/products/futebol-chuteira-society-nike.svg',
+    titulo: 'Chuteira Society Nike',
+    estrelas: 5,
+    precoCartao: 259.90
+  },
+  {
+    imagem: './assets/products/futebol-chuteira-society-nike-2.svg',
+    titulo: 'Chuteira Society Nike',
+    estrelas: 5,
+    precoCartao: 229.90
+  },
+  {
+    imagem: './assets/products/futebol-chuteira-society-track-oxn.svg',
+    titulo: 'Chuteira Society Oxn Track',
+    estrelas: 5,
+    precoCartao: 109.90
+  },
+  {
+    imagem: './assets/products/futebol-chuteira-society-track-oxn-2.svg',
+    titulo: 'Chuteira Society Oxn Track',
+    estrelas: 5,
+    precoCartao: 109.90
+  },
+
+  // FIM CHUTEIRA
+  // INICIO BOLA DE FUTEBOL
+
   {
     imagem: './assets/products/futebol-bola-de-futebol-de-campo-s11-torneio.svg',
     titulo: 'Bola de Futebol de campo S11 Torneio',
@@ -74,6 +125,22 @@ const produtos = [
     precoCartao: 219.90
   },
   {
+    imagem: './assets/products/futebol-bola-de-campo-aerotrack-uhlsport.svg',
+    titulo: 'Bola Uhlsport Campo Aerotrack',
+    estrelas: 5,
+    precoCartao: 139.90
+  },
+  {
+    imagem: './assets/products/futebol-bola-de-campo-smash-poker.svg',
+    titulo: 'Bola Poker De Campo Smash',
+    estrelas: 5,
+    precoCartao: 139.90
+  },
+
+  // FIM BOLA DE FUTEBOL
+  // INICIO BOLA DE HANDEBOL
+
+  {
     imagem: './assets/products/handbol-bola-de-handbol-iniciacao-10-penalty.svg',
     titulo: 'Bola Iniciação 10 Penalty',
     estrelas: 5,
@@ -97,11 +164,39 @@ const produtos = [
     estrelas: 5,
     precoCartao: 76.00
   },
+
+  // FIM BOLA DE HANDEBOL
+  // INICIO BOLA DE VOLEI
+
   {
-    imagem: './assets/products/basket-bola-de-basket-penalty.svg',
-    titulo: 'Bola de Basquete Oficial Penalty',
+    imagem: './assets/products/volei-bola-extreme-uhlsport.svg',
+    titulo: 'Bola Uhlsport Vôlei Extreme',
     estrelas: 5,
-    precoCartao: 129.90
+    precoCartao: 119.90
+  },
+  {
+    imagem: './assets/products/volei-bola-training-neon-poker.svg',
+    titulo: 'Bola Poker Vôlei Training Neon',
+    estrelas: 5,
+    precoCartao: 117.00
+  },
+  {
+    imagem: './assets/products/volei-bola-v330w-mikasa.svg',
+    titulo: 'Bola Mikasa Vôlei V330W',
+    estrelas: 5,
+    precoCartao: 499.90
+  },
+  {
+    imagem: './assets/products/volei-bola-vlp200-poker.svg',
+    titulo: 'Bola Poker Vôlei VLP200',
+    estrelas: 5,
+    precoCartao: 125.00
+  },
+  {
+    imagem: './assets/products/volei-bola-volei-de-praia-vxs-mikasa.svg',
+    titulo: 'Bola Mikasa Vôlei Praia VXS',
+    estrelas: 5,
+    precoCartao: 239.90
   },
   {
     imagem: './assets/products/volei-bola-de-voley-mg-3600.svg',
@@ -116,29 +211,68 @@ const produtos = [
     precoCartao: 179.90
   },
   {
-    imagem: './assets/products/diverso-agulha-penalty.svg',
+    imagem: './assets/products/volei-bola-futvolei-swl310-mikasa.svg',
+    titulo: 'Bola Mikasa Futvolei SWL310',
+    estrelas: 5,
+    precoCartao: 439.90
+  },
+
+  // FIM BOLA DE VOLEI
+  // INICIO BOLA DE BASKET
+
+  {
+    imagem: './assets/products/basket-bola-de-basket-penalty.svg',
+    titulo: 'Bola de Basquete Oficial Penalty',
+    estrelas: 5,
+    precoCartao: 129.90
+  },
+  {
+    imagem: './assets/products/basket-bola-indoor-poker.svg',
+    titulo: 'Bola Poker Indoor Basketball',
+    estrelas: 5,
+    precoCartao: 84.00
+  },
+  {
+    imagem: './assets/products/jogos-tabela-chicago-scalibu.svg',
+    titulo: 'Tabela Chicago Scalibu',
+    estrelas: 5,
+    precoCartao: 425.00
+  },
+
+  // FIM BOLA DE BASKET
+  // INICIO ACESSORIOS
+
+  {
+    imagem: './assets/products/jogos-colete-penalty.svg',
+    titulo: 'Colete Penalty',
+    estrelas: 5,
+    precoCartao: 22.00
+  },
+  {
+    imagem: './assets/products/jogos-agulha-penalty.svg',
     titulo: 'Agulha Penalty',
     estrelas: 5,
     precoCartao: 49.90
   },
   {
-    imagem: './assets/products/diverso-agulha.svg',
+    imagem: './assets/products/jogos-agulha.svg',
     titulo: 'Agulhas Para Encher Bola (Por Unidade)',
     estrelas: 5,
     precoCartao: 5.00
   },
   {
-    imagem: './assets/products/diverso-agulha.svg',
-    titulo: 'Agulhas Para Encher Bola (Por Unidade)',
-    estrelas: 5,
-    precoCartao: 5.00
-  },
-  {
-    imagem: './assets/products/diverso-bomba-big.svg',
+    imagem: './assets/products/jogos-bomba-big.svg',
     titulo: 'Bomba Big',
     estrelas: 5,
     precoCartao: 87.00
   },
+
+  // FIM ACESSORIOS
+
+  // FIM FUTEBOL, VOLEI E BASKET
+
+  // INICIO FITNESS
+
   {
     imagem: './assets/products/fitness-barra-de-exercícios-hidrolight.svg',
     titulo: 'Barra de Exercícios Hidrolight',
@@ -200,47 +334,321 @@ const produtos = [
     precoCartao: 92.00
   },
   {
+    imagem: './assets/products/fitness-roda-exercicio-dupla-vollo.svg',
+    titulo: 'Roda De Exercício Dupla Vollo',
+    estrelas: 5,
+    precoCartao: 125.00
+  },
+  {
     imagem: './assets/products/fitness-rolo-de-liberacao-miosfacial-hidrolight.svg',
     titulo: 'Rolo de Liberação Hidrolight',
     estrelas: 5,
     precoCartao: 135.00
   },
   {
-    imagem: './assets/products/diverso-bota-ortopedica-hidrolight.svg',
+    imagem: './assets/products/fitness-tapete-de-exercicio-hidrolight.svg',
+    titulo: 'Tapete de Exercícios Hidrolight',
+    estrelas: 5,
+    precoCartao: 89.90
+  },
+  {
+    imagem: './assets/products/fitness-extensor-medio-vollo.svg',
+    titulo: 'Extensor Nível Médio Vollo',
+    estrelas: 5,
+    precoCartao: 95.00
+  },
+  {
+    imagem: './assets/products/fitness-handgrip-regulavel-hidrolight.svg',
+    titulo: 'HandGrip Regulavel Hidrolight',
+    estrelas: 5,
+    precoCartao: 71.00
+  },
+  {
+    imagem: './assets/products/fitness-bota-ortopedica-hidrolight.svg',
     titulo: 'Bota Ortopédica Hidrolight',
     estrelas: 5,
     precoCartao: 129.90
   },
   {
-    imagem: './assets/products/diverso-colete-penalty.svg',
-    titulo: 'Colete Penalty',
-    estrelas: 5,
-    precoCartao: 22.00
-  },
-  {
-    imagem: './assets/products/diverso-corretor-postural-lean-hidrolight.svg',
+    imagem: './assets/products/fitness-corretor-postural-lean-hidrolight.svg',
     titulo: 'Corretor Postural Hidrolight',
     estrelas: 5,
     precoCartao: 75.00
   },
   {
-    imagem: './assets/products/diverso-joelheira-relieve-hidrolight.svg',
+    imagem: './assets/products/fitness-joelheira-relieve-hidrolight.svg',
     titulo: 'Joelheira Relieve Hidrolight',
     estrelas: 5,
     precoCartao: 129.90
   },
   {
-    imagem: './assets/products/diverso-splint-bilateral-hidrolight.svg',
+    imagem: './assets/products/fitness-joelheira-cross-hidrolight.svg',
+    titulo: 'Joelheira Cross Hidrolight',
+    estrelas: 5,
+    precoCartao: 109.90
+  },
+  {
+    imagem: './assets/products/fitness-splint-bilateral-hidrolight.svg',
     titulo: 'Splint Bilateral Hidrolight',
     estrelas: 5,
     precoCartao: 69.90
   },
   {
-    imagem: './assets/products/diverso-tapete-de-exercicio-hidrolight.svg',
-    titulo: 'Tapete de Exercícios Hidrolight',
+    imagem: './assets/products/fitness-luva-neopreme-hidrolight.svg',
+    titulo: 'Luva Neopreme Hidrolight',
     estrelas: 5,
-    precoCartao: 89.90
+    precoCartao: 52.00
   },
+  {
+    imagem: './assets/products/fitness-cinturao-gel-prottector.svg',
+    titulo: 'Cinturão Em gel Prottector',
+    estrelas: 5,
+    precoCartao: 199.90
+  },
+  {
+    imagem: './assets/products/fitness-barra-maciça-1,20cm.svg',
+    titulo: 'Barra Maciça de 1,20cm, Marca Rei Dos Fitness',
+    estrelas: 5,
+    precoCartao: 349.90
+  },
+  {
+    imagem: './assets/products/fitness-barra-oca-1,50cm.svg',
+    titulo: 'Barra Oca 1,50, Rei Dos Fitness',
+    estrelas: 5,
+    precoCartao: 169.90
+  },
+  {
+    imagem: './assets/products/fitness-barra-w-articulada.svg',
+    titulo: 'Barra W Articulada, Marca Rei Dos Fitness',
+    estrelas: 5,
+    precoCartao: 145.00
+  },
+  {
+    imagem: './assets/products/fitness-barra-w-maciça.svg',
+    titulo: 'Barra W maciça, Marca Rei Dos Fitness',
+    estrelas: 5,
+    precoCartao: 339.90
+  },
+  {
+    imagem: './assets/products/fitness-anilha-pintado.svg',
+    titulo: 'Anilha Pintado, marca rei dos fitness (Valor por KG)',
+    estrelas: 5,
+    precoCartao: 20.00
+  },
+  {
+    imagem: './assets/products/fitness-halter-pintador.svg',
+    titulo: 'Halter Pintado, marca rei dos fitness (Valor por KG)',
+    estrelas: 5,
+    precoCartao: 20.00
+  },
+  {
+    imagem: './assets/products/fitness-kettlebell-pintado.svg',
+    titulo: 'Kettlebell Pintado, marca rei dos fitness (Valor por KG)',
+    estrelas: 5,
+    precoCartao: 26.00
+  },
+
+  // FIM FITNESS
+  // INICIO ARTES MARCIAIS
+
+  {
+    imagem: './assets/products/artes-marciais-kimono-trancado-flex.svg',
+    titulo: 'Kimono Trançado Flex Torah',
+    estrelas: 5,
+    precoCartao: 559.90
+  },
+  {
+    imagem: './assets/products/artes-marciais-saco-de-bater-knockout.svg',
+    titulo: 'Saco De Bater Knockout',
+    estrelas: 5,
+    precoCartao: 219.90
+  },
+
+  // FIM ARTES MARCIAIS
+  // INICIO DANÇA
+
+  {
+    imagem: './assets/products/danca-collant-sem-manga-com-saia.svg',
+    titulo: 'Collant com saia, sem manga',
+    estrelas: 5,
+    precoCartao: 76.00
+  },
+  {
+    imagem: './assets/products/danca-fita-de-ballet-para-cabelo.svg',
+    titulo: 'Fita para Cabelo',
+    estrelas: 5,
+    precoCartao: 12.00
+  },
+  {
+    imagem: './assets/products/danca-rede-para-cabelo.svg',
+    titulo: 'Rede para cabelo Capezio',
+    estrelas: 5,
+    precoCartao: 15.00
+  },
+  {
+    imagem: './assets/products/danca-saia-jersey-infantil.svg',
+    titulo: 'Saia Jersey infantil',
+    estrelas: 5,
+    precoCartao: 49.00
+  },
+  {
+    imagem: './assets/products/danca-sapatilha-sintetica-capezio.svg',
+    titulo: 'Sapatilha Sintética Capezio',
+    estrelas: 5,
+    precoCartao: 45.00
+  },
+
+  // FIM DANÇA
+  // INICIO JOGOS COM RAQUETE
+
+  {
+    imagem: './assets/products/badmington-kit-2-raquete-3-peteca-training-set-vollo.svg',
+    titulo: 'Kit Badminton 2 Raquete+3 Peteca Training Set Vollo',
+    estrelas: 5,
+    precoCartao: 139.90
+  },
+  {
+    imagem: './assets/products/jogos-peteca-badmington-vollo.svg',
+    titulo: 'Peteca de Badminton de alta performance vollo',
+    estrelas: 5,
+    precoCartao: 12.00
+  },
+  {
+    imagem: './assets/products/jogos-peteca-standard-scalibu.svg',
+    titulo: 'Peteca Standard Scalibu',
+    estrelas: 5,
+    precoCartao: 18.00
+  },
+  {
+    imagem: './assets/products/beachtennis-kit-madeira-pro-scalibu.svg',
+    titulo: 'Kit Beach Tennis Pro Scalibu Madeira',
+    estrelas: 5,
+    precoCartao: 489.90
+  },
+  {
+    imagem: './assets/products/jogos-bolas-tenis-praia-vollo.svg',
+    titulo: 'Bolas de tênis de praia vollo',
+    estrelas: 5,
+    precoCartao: 69.90
+  },
+  {
+    imagem: './assets/products/frescobol-kit-madeira-vip-scalibu.svg',
+    titulo: 'Kit Frescobol Scalibu Vip Madeira',
+    estrelas: 5,
+    precoCartao: 54.00
+  },
+  {
+    imagem: './assets/products/pingpong-kit-1-raquete-2-bolinha-butterfly.svg',
+    titulo: 'Kit 1 Raquete+2 Bolinha Butterfly Biriba II-1',
+    estrelas: 5,
+    precoCartao: 419.90
+  },
+  {
+    imagem: './assets/products/pingpong-kit-2-raquete-3-bolinha-vollo-leisure.svg',
+    titulo: 'Kit 2 Raquete+3 bolinha Vollo Leisure',
+    estrelas: 5,
+    precoCartao: 96.00
+  },
+  {
+    imagem: './assets/products/pingpong-raquete-advanced-poker.svg',
+    titulo: 'Raquete Poker Advanced',
+    estrelas: 5,
+    precoCartao: 52.00
+  },
+  {
+    imagem: './assets/products/pingpong-raquete-donic.svg',
+    titulo: 'Raquete Donic',
+    estrelas: 5,
+    precoCartao: 79.90
+  },
+  {
+    imagem: './assets/products/pingpong-raquete-lin-yun-ju-butterfly.svg',
+    titulo: 'Raquete Tênis De Mesa Butterfly Lin Yun-ju s-2',
+    estrelas: 5,
+    precoCartao: 269.90
+  },
+  {
+    imagem: './assets/products/pingpong-kit-bola-tenis-de-mesa.svg',
+    titulo: 'Kit com 6 bolas de tênis de mesa vollo',
+    estrelas: 5,
+    precoCartao: 28.00
+  },
+  {
+    imagem: './assets/products/pingpong-rede-tenis-mesa-vollo.svg',
+    titulo: 'Conjunto de rede e suporte para Tênis de mesa Vollo',
+    estrelas: 5,
+    precoCartao: 119.90
+  },
+  {
+    imagem: './assets/products/jogos-taco-baisebol-vollo.svg',
+    titulo: 'Taco de Beisebol vollo',
+    estrelas: 5,
+    precoCartao: 235.00
+  },
+  {
+    imagem: './assets/products/jogos-bola-baisebol-vollo.svg',
+    titulo: 'Bola De Beisebol vollo',
+    estrelas: 5,
+    precoCartao: 65.00
+  },
+
+  // FIM JOGOS COM RAQUETE
+  // INICIO NATAÇÃO
+
+  {
+    imagem: './assets/products/natacao-maio-hammerhead.svg',
+    titulo: 'Maio Hammerhead',
+    estrelas: 5,
+    precoCartao: 129.90
+  },
+  {
+    imagem: './assets/products/natacao-oculos-infantil.svg',
+    titulo: 'Óculos infantis (A partir de)',
+    estrelas: 5,
+    precoCartao: 39.90
+  },
+  {
+    imagem: './assets/products/natacao-poli-boia-hammerhead.svg',
+    titulo: 'Poli Boia Hammerhead',
+    estrelas: 5,
+    precoCartao: 65.00
+  },
+
+  // FIM NATAÇÃO
+  // INICIO JOGOS DIVERSOS
+
+  {
+    imagem: './assets/products/jogos-futebol-botao.svg',
+    titulo: 'Jogo de Botão Crakes',
+    estrelas: 5,
+    precoCartao: 149.90
+  },
+  {
+    imagem: './assets/products/jogos-bingo-com-globo-cromado.svg',
+    titulo: 'Bingo N2 Scalibu Com Globo cromado',
+    estrelas: 5,
+    precoCartao: 475.00
+  },
+  {
+    imagem: './assets/products/jogos-domino.svg',
+    titulo: 'Dominó Preto e Branco',
+    estrelas: 5,
+    precoCartao: 169.90
+  },
+  {
+    imagem: './assets/products/jogos-fichas-poker.svg',
+    titulo: 'Jogo de Fichas Poker Chips',
+    estrelas: 5,
+    precoCartao: 65.00
+  },
+  {
+    imagem: './assets/products/jogos-xadrez-dama-trilha.svg',
+    titulo: 'Jogo com Xadrez, Dama e Trilha, Marca pangue',
+    estrelas: 5,
+    precoCartao: 75.00
+  },
+
+  // FIM JOGOS DIVERSOS
 ];
 
 const produtosContainer = document.getElementById('produtos');
@@ -248,14 +656,14 @@ produtos.forEach(produto => {
   const precoPix = (produto.precoCartao * 0.97).toFixed(2);
   const produtoHtml = `
                 <div class="flex flex-col justify-items-start bg-blue-100 p-0 rounded-lg">
-                  <img src="${produto.imagem}" alt="${produto.titulo}" class="rounded-lg mb-2 max-w-lg">
+                  <img src="${produto.imagem}" alt="${produto.titulo}" class="rounded-lg mb-2 max-w-lg bg-white">
                   <p class="font-medium mb-2 px-4">${produto.titulo}</p>
                   <div class="flex items-center mb-4 px-4">
                     ${Array.from({ length: produto.estrelas }).map(() => '<i class="fa-solid fa-star" style="color: #daa81d;"></i>').join('')}
                   </div>
                   <p class="mb-1 px-4">R$${precoPix} no Pix ou Boleto</p>
                   <p class="font-thin text-sm mb-4 px-4">ou R$${produto.precoCartao.toFixed(2)} no cartão de crédito</p>
-                  <button class="border mx-4 mb-4 rounded-lg px-10 py-4 bg-green-600 text-white font-medium hover:bg-green-800 md:max-lg:px-4 max-sm:px-1">SAIBA MAIS</button>
+                  <a class="cursor-pointer text-center pointer border mx-4 mb-4 rounded-lg px-10 py-4 bg-green-600 text-white font-medium hover:bg-green-800 md:max-lg:px-4 max-sm:px-1" href="https://api.whatsapp.com/send?phone=5561996131441&text=Gostaria%20de%20saber%20mais%20sobre%20${produto.titulo}." target="_blank">SAIBA MAIS</a>
                 </div>
               `;
   produtosContainer.insertAdjacentHTML('beforeend', produtoHtml);
